436376adb36af96d58f2db6a58014a77aa23a802
Jun Nakajima <ikuta1919@gmail.com>
2021-08-01 20:13:33 +0900

    グローバル変数によるデータ受け渡し

---
+++ chap07/7.3/q/7-2.c
@@ -0,0 +1,25 @@
+//
+// Created by jun on 2021/08/01.
+//
+
+#include <stdio.h>
+#include <pthread.h>
+
+int x; // グローバルな変数
+float y;
+
+void Func2(void){
+    printf("new thread: x = %d y = %f\n", x, y);
+}
+
+int main(int argc, char *argv[]){
+    pthread_t th_id;
+    int st;
+    x=-1;
+    y=0.1;
+    st = pthread_create(&th_id, NULL, (void *(*)(void *))Func2, NULL);
+    if(st != 0) return 1;
+    pthread_join(th_id, NULL);
+    return 0;
+}
+
